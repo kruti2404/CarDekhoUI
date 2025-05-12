18a05ec277f1e575cf0a32babfd011336c18de74
Kruti <krutipatel02004@gmail.com>
2025-05-12 11:08:29 +0530

    Changed the navbar and header

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavabrComponent } from '../../shared/components/navabr/navabr.component';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../../shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavabrComponent, RouterOutlet],
+  imports: [NavabrComponent, RouterOutlet, SidebarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
