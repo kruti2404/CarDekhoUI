4aaaa3908525036161ab67a407b2714883600132
Kruti <krutipatel02004@gmail.com>
2025-05-12 19:08:51 +0530

    Added some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
-import {  Routes,  } from '@angular/router';
+import { Routes, } from '@angular/router';
 import { EditComponent } from './features/edit/edit.component';
 import { GetVehicleByIdComponent } from './features/get-vehicle-by-id/get-vehicle-by-id.component';
 import { FormfilterpartialComponent } from './features/formfilterpartial/formfilterpartial.component';
 import { HomeComponent } from './features/home/home.component';
 import { VehicleDetailsComponent } from './features/vehicle-details/vehicle-details.component';
 import { CreateComponent } from './features/create/create.component';
+import { LoginComponent } from './features/login/login.component';
 
 export const routes: Routes = [
 
@@ -14,4 +15,5 @@ export const routes: Routes = [
     { path: 'Filters', component: FormfilterpartialComponent },
     { path: 'create', component: CreateComponent },
     { path: 'Edit/:id', component: EditComponent },
+    { path: 'login', component: LoginComponent }
 ]
--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private Apiurl = environment.apiurls;
+  constructor(private http: HttpClient) { }
+  login(Username: string , Password : string){
+    return this.http.post(`${this.Apiurl}/Auth/login`, {Username, Password})
+  }
+  register(Username: string , Password : string){
+    return this.http.post(`${this.Apiurl}/Auth/register`, {Username, Password})
+  }
 
-  constructor() { }
 }
